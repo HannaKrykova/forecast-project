8fdb4f5739326235cd889772169b1f6de744968d
HannaKrykova <hannakrykova@gmail.com>
2023-12-30 17:43:57 +0200

    Added API and search function update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-function citySearch(event) {
-  event.preventDefault();
+function weatherInfoUpdate(response) {
+  let currentTemperature = document.querySelector(
+    "#forecast-temprature-tempvalue"
+  );
   let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = response.data.city;
+  currentTemperature.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function citySearch(city) {
+  let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(weatherInfoUpdate);
+}
+
+function citySubmit(event) {
+  event.preventDefault();
   let searchedCity = document.querySelector("#searched-city");
-  currentCity.innerHTML = searchedCity.value;
+  citySearch(searchedCity.value);
 }
 
 let citySearchForm = document.querySelector("#search-form");
-citySearchForm.addEventListener("submit", citySearch);
+citySearchForm.addEventListener("submit", citySubmit);
