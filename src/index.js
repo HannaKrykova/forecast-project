f6e2e04cbaf8f1d444d77530658533da48498972
HannaKrykova <hannakrykova@gmail.com>
2023-12-30 18:54:01 +0200

    Added more weather function update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 function weatherInfoUpdate(response) {
-  let currentTemperature = document.querySelector(
-    "#forecast-temprature-tempvalue"
-  );
-  let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = response.data.city;
-  currentTemperature.innerHTML = Math.round(response.data.temperature.current);
+  let temperature = document.querySelector("#forecast-temprature-tempvalue");
+  let city = document.querySelector("#current-city");
+  let weatherDescription = document.querySelector("#weather-description");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let time = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  city.innerHTML = response.data.city;
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  weatherDescription.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+  time.innerHTML = formatDate(date);
 }
 
 function citySearch(city) {
@@ -21,3 +28,26 @@ function citySubmit(event) {
 
 let citySearchForm = document.querySelector("#search-form");
 citySearchForm.addEventListener("submit", citySubmit);
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
